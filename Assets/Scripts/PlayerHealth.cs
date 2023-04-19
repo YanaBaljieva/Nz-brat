267ef93f73a76349897701fdef9fcfaac7941390
YanaBaljieva <yanabaljieva@gmail.com>
2023-04-19 23:25:16 +0300

    added first level

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float health;
+    public float maxHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       //maxHealth  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
